e111492f3289b7c07b5304149c1c40606c9d8c82
johnnyfsa <joaofeirense@gmail.com>
2024-05-28 09:21:57 -0300

    test commit

--- src/main/java/br/com/biscoithor/screenmatch/main/Main.java
+++ src/main/java/br/com/biscoithor/screenmatch/main/Main.java
@@ -86,5 +86,6 @@ public class Main {
                 .filter(e->e.getImdbRating()>0.0)
                 .collect(Collectors.summarizingDouble(Episode::getImdbRating));
         System.out.println(est);
+        System.out.println("teste");
     }
 }
